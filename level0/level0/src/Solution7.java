de3969d3d8fcf9c967c27272b99742bad6d49ba8
charcoal <devsj3586@gmail.com>
2022-12-03 18:47:43 +0900

    2022.12.03 algorithm example upload

---
+++ level0/level0/src/Solution7.java
@@ -0,0 +1,20 @@
+// 각도기 문제
+class Solution7 {
+    public int solution(int angle) {
+        if (0 < angle && angle < 90) {
+            return 1;
+        } else if (angle == 90) {
+            return 2;
+        } else if (90 < angle && angle < 180) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
+/* 삼항 연산자
+class Solution {
+    public int solution(int angle) {
+        return angle == 180 ? 4 : angle < 90 ? 1 : angle == 90 ? 2 : angle > 90 ? 3 : 0;
+    }
+}*/
---
+++ level0/level0/src/Solution8.java
@@ -0,0 +1,7 @@
+// 두 수의 나눗셈 ( 형변화)
+class Solution8 {
+    public int solution(int num1, int num2) {
+        float answer = (float) num1 / num2 * 1000;
+        return (int) answer;
+    }
+}
