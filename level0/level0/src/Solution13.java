8f6b3ff4dd4d267a4d341f0904e5ef85922b4c83
charcoal <devsj3586@gmail.com>
2022-12-07 16:58:04 +0900

    2022.12.05 algorithm 프로그래머스 example(머쓱이 보다 키큰사람)

---
+++ level0/level0/src/Solution13.java
@@ -0,0 +1,21 @@
+//  머쓱이 보다 키큰사람
+class Solution13 {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (height < array[i]) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
+
+// foreach 문과 삼항 연산자 사용
+//    public int solution(int[] array, int height) {
+//        int answer = 0;
+//        for (int i : array) {
+//            answer += (i > height) ? 1 : 0;
+//        }
+//        return answer;
+//    }
